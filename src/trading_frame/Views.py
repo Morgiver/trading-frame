9039dc6f86399dcdee30eb90f6f1e43faa80f5a1
Morgiver <morgan@morgiver.net>
2023-04-26 10:29:19 +0200

    Delete non finish work in main branch

--- src/trading_frame/Views.py
+++
@@ -1,9 +0,0 @@
-# Custom Python
-from src.trading_frame.Frames import Frame
-
-class View:
-    def __init__(self, frame: Frame) -> None:
-        self.frame = frame
-
-    def set_frame(self, frame: Frame):
-        pass
--- src/trading_frame/__init__.py
+++ src/trading_frame/__init__.py
@@ -1,18 +1,2 @@
 # Custom Python
-from src.trading_frame.Factory import ViewFactory
-from src.trading_frame.Views import View
-from src.trading_frame.Frames import Tick, Trade
-
-class ViewManager:
-    def __init__(self):
-        self.views = {}
-
-    def add(self, name: str, builded_view: View):
-        if not name in self.views:
-            self.views[name] = builded_view
-            return self
-
-        raise Exception(f"A view with name : [{name}] already exist")
-
-    def feed(self, raw_data: list, _type: str = "tick"):
-        pass
+from src.trading_frame.Frames import *
